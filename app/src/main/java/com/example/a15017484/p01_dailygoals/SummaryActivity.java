01a29e3bd881956603fc27d1cb050fbcec0d8846
actuallycoding <15017484@myrp.edu.sg>
2017-04-18 13:54:58 +0800

    Added the Done button to close the Summary Activity.

--- app/src/main/java/com/example/a15017484/p01_dailygoals/SummaryActivity.java
+++ app/src/main/java/com/example/a15017484/p01_dailygoals/SummaryActivity.java
@@ -3,6 +3,8 @@ package com.example.a15017484.p01_dailygoals;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SummaryActivity extends AppCompatActivity {
@@ -19,5 +21,12 @@ public class SummaryActivity extends AppCompatActivity {
         TextView tv1 = (TextView) findViewById(R.id.textViewSummary);
         // Display the name and age on the TextView
         tv1.setText("Read up on materials before class: " + info[0] + "\nArrive on time so as not to miss important part of the lesson: " + info[1] + "\nAttempt the problem myself: " + info[2] + "\nReflection: " + info[3]);
+        Button btnDone = (Button) findViewById(R.id.buttonDone);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
